49a505561b461296cceeb6c5036d00b3aa4c61bc
Litorian113 <154338974+Litorian113@users.noreply.github.com>
2025-02-18 02:28:45 +0100

    Entferne index.js und integriere Globe-Komponente in die Startseite

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
-<p>Test :D</p>
+<script>
+    import Globe from "../lib/Globe.svelte";
+  </script>
+  
+  <main>
+    <Globe />
+  </main>
+  
